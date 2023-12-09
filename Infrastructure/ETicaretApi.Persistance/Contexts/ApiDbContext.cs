129a2eeaa70b210c99d35e11eccbe00472a6fb73
Bugra Okumus <bugra.okumus@d-teknoloji.com.tr>
2023-12-09 15:51:17 +0300

    feat : auto create date integration

--- Infrastructure/ETicaretApi.Persistance/Contexts/ApiDbContext.cs
+++ Infrastructure/ETicaretApi.Persistance/Contexts/ApiDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretApi.Domain.Entities;
+using ETicaretApi.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,24 @@ namespace ETicaretApi.Persistance.Contexts
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<Order> Orders { get; set; } 
+        public DbSet<Order> Orders { get; set; }
+
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker
+                .Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdateDate = DateTime.UtcNow
+                };
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
---
+++ Presentation/E-TicaretApi.API/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using ETicaretApi.Application.Repositories.OrderRepositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_TicaretApi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+        private readonly IOrderWriteRepository _orderWriteRepository;
+
+        public OrderController(IOrderWriteRepository orderWriteRepository)
+        {
+            _orderWriteRepository = orderWriteRepository;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Test()
+        {
+            _orderWriteRepository.AddAsync(new(){Adress = "Adress 1",Description = "Description 1",});
+            _orderWriteRepository.AddAsync(new(){Adress = "Adress 2",Description = "Description 2",});
+
+            await _orderWriteRepository.SaveAsync();
+            return Ok();
+        }
+    }
+}
